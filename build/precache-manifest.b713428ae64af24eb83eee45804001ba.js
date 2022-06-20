a9e51b4c93cf941da9be72547a3cd4b4e9e72b74
Ramin Hbb <raminhbb.au@gmail.com>
2022-06-20 14:50:37 +0430

    publish gh-page

---
+++ build/precache-manifest.b713428ae64af24eb83eee45804001ba.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8414f9640c464fefac4c",
+    "url": "/GoldCoastYard/static/js/0.8414f964.chunk.js"
+  },
+  {
+    "revision": "93b78e53c6910aa698eb",
+    "url": "/GoldCoastYard/static/js/1.93b78e53.chunk.js"
+  },
+  {
+    "revision": "6dcab5821f43ed789ceb",
+    "url": "/GoldCoastYard/static/js/2.6dcab582.chunk.js"
+  },
+  {
+    "revision": "b05ec2eb2e6bb3fab379",
+    "url": "/GoldCoastYard/static/js/main.b05ec2eb.chunk.js"
+  },
+  {
+    "revision": "996f7a7ca3f12e67a2a0",
+    "url": "/GoldCoastYard/static/js/4.996f7a7c.chunk.js"
+  },
+  {
+    "revision": "b0890257a4c7599a09d4",
+    "url": "/GoldCoastYard/static/js/5.b0890257.chunk.js"
+  },
+  {
+    "revision": "89c8d563daedfacf71ed",
+    "url": "/GoldCoastYard/static/js/6.89c8d563.chunk.js"
+  },
+  {
+    "revision": "3efc13c0871c089d2bc9",
+    "url": "/GoldCoastYard/static/js/7.3efc13c0.chunk.js"
+  },
+  {
+    "revision": "3ceeb574705316e62b85",
+    "url": "/GoldCoastYard/static/js/8.3ceeb574.chunk.js"
+  },
+  {
+    "revision": "050d5dfa6e6ba90730cd",
+    "url": "/GoldCoastYard/static/css/9.b4ad74fc.chunk.css"
+  },
+  {
+    "revision": "050d5dfa6e6ba90730cd",
+    "url": "/GoldCoastYard/static/js/9.050d5dfa.chunk.js"
+  },
+  {
+    "revision": "78903d7cc0259abb6bbd",
+    "url": "/GoldCoastYard/static/js/10.78903d7c.chunk.js"
+  },
+  {
+    "revision": "d4ee4174ee07f3b952fe",
+    "url": "/GoldCoastYard/static/js/runtime~main.d4ee4174.js"
+  },
+  {
+    "revision": "46f4e238e7026ef3b23be6905d18fb42",
+    "url": "/GoldCoastYard/index.html"
+  }
+];
