0657f2eb818451cba50ce5053d553ec653cd4007
Ramin Hbb <raminhbb.au@gmail.com>
2022-06-20 08:38:04 +0430

    upload build branch

--- build/precache-manifest.a3fd5ca0e76119b78605ad358904e70b.js
+++ build/precache-manifest.a3fd5ca0e76119b78605ad358904e70b.js
@@ -12,16 +12,16 @@ self.__precacheManifest = [
     "url": "/GoldCoastYard/static/js/2.146c1f53.chunk.js"
   },
   {
-    "revision": "1f1f26ddf929f900fd58",
-    "url": "/GoldCoastYard/static/js/main.1f1f26dd.chunk.js"
+    "revision": "539feec396f9f3141ace",
+    "url": "/GoldCoastYard/static/js/main.539feec3.chunk.js"
   },
   {
-    "revision": "5ac42c207607710cd884",
-    "url": "/GoldCoastYard/static/js/4.5ac42c20.chunk.js"
+    "revision": "6091335427ad08df016b",
+    "url": "/GoldCoastYard/static/js/4.60913354.chunk.js"
   },
   {
-    "revision": "64c9972ae59373f03204",
-    "url": "/GoldCoastYard/static/js/5.64c9972a.chunk.js"
+    "revision": "01e2ef14766fa08a5ba3",
+    "url": "/GoldCoastYard/static/js/5.01e2ef14.chunk.js"
   },
   {
     "revision": "01d6c2a11497f300f58e",
@@ -36,31 +36,31 @@ self.__precacheManifest = [
     "url": "/GoldCoastYard/static/js/8.a978eb17.chunk.js"
   },
   {
-    "revision": "ef820364db244aebb276",
-    "url": "/GoldCoastYard/static/js/9.ef820364.chunk.js"
+    "revision": "232e71be11758e86e60b",
+    "url": "/GoldCoastYard/static/js/9.232e71be.chunk.js"
   },
   {
     "revision": "4de03710e7765cddf932",
     "url": "/GoldCoastYard/static/js/10.4de03710.chunk.js"
   },
   {
-    "revision": "ba8eecd58961693e8751",
+    "revision": "8ca73fdd9bf7035131fa",
     "url": "/GoldCoastYard/static/css/11.e84dadb9.chunk.css"
   },
   {
-    "revision": "ba8eecd58961693e8751",
-    "url": "/GoldCoastYard/static/js/11.ba8eecd5.chunk.js"
+    "revision": "8ca73fdd9bf7035131fa",
+    "url": "/GoldCoastYard/static/js/11.8ca73fdd.chunk.js"
   },
   {
-    "revision": "25bcc075bd701331a549",
-    "url": "/GoldCoastYard/static/js/12.25bcc075.chunk.js"
+    "revision": "44b9f7d8a568d90b5766",
+    "url": "/GoldCoastYard/static/js/12.44b9f7d8.chunk.js"
   },
   {
-    "revision": "c281e88b0f78719e9c77",
-    "url": "/GoldCoastYard/static/js/runtime~main.c281e88b.js"
+    "revision": "67e4697248702022fff6",
+    "url": "/GoldCoastYard/static/js/runtime~main.67e46972.js"
   },
   {
-    "revision": "c8393855e1e12b34e5b382fdd9ac83d2",
+    "revision": "3c5c120774cecea36ad7fef02d510652",
     "url": "/GoldCoastYard/index.html"
   }
 ];
