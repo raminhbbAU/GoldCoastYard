14b176d1d0d0b435c11a62259498b571266b8bd4
raminhbbAU <raminhbb.au@gmail.com>
2022-07-20 01:25:50 +0430

    fix UI issues on ServiceWithFullImage

--- src/components/ServiceWithFullImage/index.tsx
+++ src/components/ServiceWithFullImage/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Row, Col } from "antd";
 import { withTranslation } from "react-i18next";
 import { Slide } from "react-awesome-reveal";
-import { ServiceSection,ServiceName, PageTitle,CardSection } from "./styles";
+import { ServiceSection,ServiceName, PageTitle,CardSection, CardHolder, CardsHolder, ImageHolder } from "./styles";
 import { useHistory } from "react-router-dom";
 
 interface Props {
@@ -21,38 +21,25 @@ const ServiceWithFullImage = ({ title, services, id, t }: Props) => {
   };
 
   const serviceList = services.map((member: any) => (    
-    <Col lg={12} md={24} sm={24} xs={24}>
-      <CardSection src={member.img} onClick={() => navigateTo(member.link)}>
+    <Col lg={12} md={12} sm={24} xs={24}>
+      <CardsHolder>         
+        <ImageHolder src={member.img} alt={member.name}></ImageHolder>
         <ServiceName>{member.name}</ServiceName>
-        {/* <Content>{member.description}</Content>       */}
-      </CardSection>
+      </CardsHolder>
     </Col>
   ));
 
   return (
     <ServiceSection id={id}>
       {/* <Slide direction="down"> */}
-        <Row>
+        {/* <Row>
           <Col lg={24} md={24} sm={24} xs={24}>
             <PageTitle>{title}</PageTitle>
           </Col>
-        </Row>
+        </Row> */}
         <Row>
           {serviceList}
-          <Col lg={24} md={0} sm={0} xs={0}>
-             <p>lg mode</p>
-          </Col>
-          <Col lg={0} md={24} sm={0} xs={0}>
-             <p>md mode</p>
-          </Col>
-          <Col lg={0} md={0} sm={24} xs={0}>
-             <p>sm mode</p>
-          </Col>
-          <Col lg={0} md={0} sm={0} xs={24}>
-             <p>xs mode</p>
-          </Col>
         </Row>
-      {/* </Slide> */}
     </ServiceSection>
   );
 };
--- src/components/ServiceWithFullImage/styles.tsx
+++ src/components/ServiceWithFullImage/styles.tsx
@@ -1,32 +1,74 @@
 import styled from "styled-components";
 
 export const ServiceSection = styled("section")`
+  margin-top:50px;
+  /* display:flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  margin-bottom:50px;
+  margin-right:8px;
+  margin-left:8px; */  
+`;
+
+export const CardsHolder = styled("div")`
+  display:flex;
+  text-align: center; 
+  justify-content: center;
+  align-items: center;
+  padding:5px;
+  opacity: 0.6;
+
+  &:hover,
+  &:active,
+  &:focus {
+    opacity: 0.9;
+  }
+`;
+
+export const ImageHolder = styled("img")<any>`
+  height: ${(p) => (p.height ? p.height : "100%")};
+  width: ${(p) => (p.width ? p.width : "100%")};
+  border-radius:8px;
+  position: relative;
+`;
+
+export const ServiceName = styled("p")<any>`
+  position: absolute;
+  top: 40%;
+  cursor: pointer;
+  opacity: 1;
+  font-size: 52px;
+  font-weight: 900;
+  font-style:bold;
+  color:#F0DA13;
+  opacity:100%;
+
+`;
+
+
+export const CardHolder = styled("div")`
+  background-color:blue;
+  margin-right:3px;
+  height:100%;
+  width:100%;
   display:flex;
   text-align: center;
   justify-content: center;
   align-items: center;
-  margin-top:50px;
-  margin-bottom:50px;
-  margin-right:8px;
-  margin-left:8px;
 `;
 
 export const CardSection = styled("div")<any>`
-  display:flex;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
   border-radius: 2%;
   padding: 4px;
   background-image: url(${(p) => (p.src ? p.src : "")});
   background-size: cover;
   background-repeat:no-repeat;
   opacity:80%;
-  width: 562px;// 375px; 
-  height: 225px; //150px;
+  width: 100%; //562px;// 375px; 
+  height: 100%; //225px; //150px;
   cursor: pointer;
   opacity: 0.6;
-  margin:15px;
 
   &:hover,
   &:active,
@@ -34,11 +76,11 @@ export const CardSection = styled("div")<any>`
     opacity: 0.9;
   }
 
-  @media only screen and (min-width: 768px) 
+  /* @media only screen and (min-width: 768px) 
   {
     width: 375px;
     height: 150px; 
-  }
+  } */
         /* @media only screen and (max-width: 768px) 
         {
             margin-right:8px;
@@ -62,7 +104,7 @@ export const PageTitle = styled("h6")`
   margin-bottom:100px;
 `;
 
-export const ServiceName = styled("p")`
+export const ServiceName2 = styled("p")`
   font-size: 52px;
   font-weight: 900;
   font-style:bold;
@@ -75,16 +117,4 @@ export const Content = styled("p")`
   font-size: 15px;
   margin-bottom:50px;
   color:#7f7f7f;
-`;
-
-
-export const ContentWrapper = styled("div")`
-  max-width: 570px;
-  display: flex;
-  justify-content: center;
-  align-items: 'center',
-
-  @media only screen and (max-width: 768px) {
-    max-width: 100%;
-  }
 `;
